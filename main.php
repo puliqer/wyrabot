58e93bf8bae282942bd583cf7c79b94572ff4ccf
MoRti <themorti@yahoo.com>
2020-06-07 15:15:05 +0430

    changing the main config part (remove bot id section)

--- main.php
+++ main.php
@@ -3,8 +3,7 @@
 #########################  Main Config Here  #########################
 
 $token = '1007063839:AAGXask-4Irff_Ka6DKcCmZs3Y5JJihy3kU'; // bot token here
-$bot = "imkind_bot"; // bot username here
-$id = 144686606; // bot id here 
+$bot = "WyRaBot"; // bot username here
 define('TOKEN',$token);
 
 // The main function for execute methods
