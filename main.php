4b3e0291fb5f95710ed23e024bdec3a09d7b90b3
MoRti <themorti@yahoo.com>
2020-06-07 14:16:50 +0430

    adding the main part of bot (token, id, username)

---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+
+$token = '4263833576:AAGB-rbVVzIFuXITZL7My6yq0DauhVj6-v0'; // bot token here
+$bot = "imkind_bot"; // bot username here
+$id = 144686606; // bot id here 
+define('TOKEN',$token);
+
+
+
+?>
