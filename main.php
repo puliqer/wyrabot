bea523444e6d76728e901978e25f88a2ecdbf64d
MoRti <themorti@yahoo.com>
2020-06-07 14:28:40 +0430

    set getUpdates method

--- main.php
+++ main.php
@@ -1,10 +1,10 @@
 <?php
 
-$token = '4263833576:AAGB-rbVVzIFuXITZL7My6yq0DauhVj6-v0'; // bot token here
+$token = '1007063839:AAGXask-4Irff_Ka6DKcCmZs3Y5JJihy3kU'; // bot token here
 $bot = "imkind_bot"; // bot username here
 $id = 144686606; // bot id here 
 define('TOKEN',$token);
 
-
+$url = "https://api.telegram.org/bot"1007063839:AAGXask-4Irff_Ka6DKcCmZs3Y5JJihy3kU/getUpdate";
 
 ?>
