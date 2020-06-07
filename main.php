80a6baf16841380cacf96a366c3c52867925c336
MoRti <themorti@yahoo.com>
2020-06-07 17:56:52 +0430

    adding more public vars (date, username, first and last name and ...)

--- main.php
+++ main.php
@@ -66,18 +66,24 @@ function sendvideo($chat_id, $video, $caption){
 
 $main = json_decode(file_get_contents('php://input')); // getting data from user
 var_dump($update);
+$update_id = $main->update_id; // getting the update id
 $message = $main->message; // getting the message full data
+$message_id = $message->message_id; // getting the message id
 $from_id = $message->from->id; // getting the user id 
 $chat_id = $message->chat->id; // getting the user id in chat
 $textid = $message->text->id; // getting the sent text id from user
 $text = $message->text; // getting the sent text
-$username = $message->form->first_name;
+$date = $message->date; // getting the date of sent message
+$first_name = $message->form->first_name; // getting the first name of user
+$last_name = $message->form->last_name; // getting the last name of user
+$username = $message->form->username; // getting the username of user
+
 
 #########################  Usable String Here  #########################
 
 if ($text == '/start') {
     sendmessage($chat_id, "
-        Welcome dear {$username} 🐱, add me to Chat and Have Fun.
+        Welcome dear {$first_name} 🐱, add me to Chat and Have Fun.
         if you dont know how to use this bot, use /help command ! 
     ");
 }
