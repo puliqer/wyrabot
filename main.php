2e346aa9fcc556a3754ef4dfae060508d1187865
MoRti <themorti@yahoo.com>
2020-06-07 14:32:09 +0430

    adding the main function for execute methods named bot()

--- main.php
+++ main.php
@@ -5,6 +5,17 @@ $bot = "imkind_bot"; // bot username here
 $id = 144686606; // bot id here 
 define('TOKEN',$token);
 
-$url = "https://api.telegram.org/bot"1007063839:AAGXask-4Irff_Ka6DKcCmZs3Y5JJihy3kU/getUpdate";
-
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".TOKEN."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL, $url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS, $datas);
+    $res = curl_exec($ch);
+    if (curl_error($ch)) {
+        var_dump(curl_error($ch));
+    } else {
+        return json_decode($res);
+    }
+}
 ?>
