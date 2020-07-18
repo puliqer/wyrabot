b19d4f61afad385b0da1489a1b96800d1350b920
MoRti <themorti@yahoo.com>
2020-07-18 15:21:19 +0430

    replace the api vaars to main.php

--- main.php
+++ main.php
@@ -21,6 +21,36 @@ function bot($method, $datas=[]){
     }
 }
 
+//-------------------------------------
+
+function send_reply($url, $post_params) {
+
+    $cu = curl_init();
+    curl_setopt($cu, CURLOPT_URL, $url);
+    curl_setopt($cu, CURLOPT_POSTFIELDS, $post_params);
+    curl_setopt($cu, CURLOPT_RETURNTRANSFER, true);  // get result
+    $result = curl_exec($cu);
+    curl_close($cu);
+    return $result;
+}
+
+$main = json_decode(file_get_contents('php://input')); // getting data from user
+var_dump($update);
+$update_id = $main->update_id; // getting the update id
+$message = $main->message; // getting the message full data
+$message_id = $message->message_id; // getting the message id
+$from_id = $message->from->id; // getting the user id 
+$chat_id = $message->chat->id; // getting the user id in chat
+$textid = $message->text->id; // getting the sent text id from user
+$text = $message->text; // getting the sent text
+$date = $message->date; // getting the date of sent message
+
+$first_name = $message->from->first_name; // getting the first name of user
+$last_name = $message->from->last_name; // getting the last name of user
+$username = $message->from->username; // getting the username of user
+$user_id = $message->from->id;
+
+
 include_once 'public/methods.php';
 
 include_once 'public/api_vars.php';
