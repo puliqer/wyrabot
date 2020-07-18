044a9c5c2a5ba443382269a2b6ca757ef7d5326d
MoRti <themorti@yahoo.com>
2020-07-18 15:30:03 +0430

    replace the gaycheck command form conditions.php to main.php

--- main.php
+++ main.php
@@ -52,6 +52,24 @@ $last_name = $message->from->last_name; // getting the last name of user
 $username = $message->from->username; // getting the username of user
 $user_id = $message->from->id;
 
+#########################  Gaycheck command (New Edition)  #########################
+
+if ($text = '/test') {
+    $reply = "this is a message";
+    $url = "https://api.telegram.org/bot1007063839:AAGXask-4Irff_Ka6DKcCmZs3Y5JJihy3kU" . "/sendMessage";
+    $post_params = [ 'chat_id' => $chat_id , 'text' => $reply ];
+ 
+    $result = send_reply($url, $post_params);
+    $result_array = json_decode($result, true);
+    $msg_id  = $result_array["result"]["message_id"];
+ 
+    sleep(3);
+ 
+    $reply = "this is updated message";
+    $url = "https://api.telegram.org/bot1007063839:AAGXask-4Irff_Ka6DKcCmZs3Y5JJihy3kU" . "/editMessageText";
+    $post_params = [ 'chat_id' => $chat_id , 'text' => $reply , 'message_id' => $msg_id ];
+    send_reply($url, $post_params);
+}
 
 include_once 'public/methods.php';
 
