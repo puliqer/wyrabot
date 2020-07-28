fd8bbeff367dab82093b2cdebf6b3b1743ddbbe1
MoRti <themorti@yahoo.com>
2020-07-28 16:32:55 +0430

    rebuild the methods

--- bot.php
+++ bot.php
@@ -129,11 +129,96 @@ if ($msg = $update->edited_message) {
 	$caption_entities = $msg->caption_entities;
 	$audio = $msg->audio;
 }
- 
+// get inline queries 
 $inline_query = $main->inline_query->query;
 $inline_query_id = $main->inline_query->id;
 
+define('FROM', $from);
+define('CHAT', $chat);
+define('MSGID', $msgid);
+
 ########## Mehtods Here ##########
+
+// sendmessage method
+function sendmessage($chat, $text, $key = '', $reply = '') {
+	return bot('sendmessage',[
+		'chat_id' => $chat,
+		'text' => $text,
+		'reply_markup' => $key,
+		'reply_to_message_id' => $reply,
+		'parse_mode' => 'markdown'
+	]);
+}
+
+//  reply message method
+function reply($chat, $text, $key = '', $reply = '') {
+	global $chat, $msgid;
+	return sendmessage($chat, $text, $key, $reply ?: $msgid);
+}
+
+// forward message method
+function forward_message($chat, $from, $msgid) {
+	return bot('forwardmessage',[
+		'chat_id' => $chat,
+		'from_chat_id' => $from,
+		'message_id' => $msgid
+	]);
+}
+
+// edit message method
+function editmessage($chat, $msgid, $text, $key = '') {
+	return bot('editmessagetext',[
+		'chat_id' => $chat,
+		'message_id' => $msgid,
+		'text' => $text,
+		'reply_markup' => $key,
+		'parse_mode' => 'markdown'
+	]);
+}
+
+function edit($text, $key = '') {
+	global $chat, $msgid;
+	return editmessage($chat, $msgid, $text, $key);
+}
+
+// sendPhoto method
+function sendphoto($chat, $photo, $caption, $reply = '') {
+    return bot('sendPhoto', [
+        'chat_id' => $chat,
+        'photo' => $photo,
+        'caption' => $caption,
+		'reply_to_message_id' => $reply,
+		'parse_mode' => 'markdown'
+    ]);
+}
+// sendAudio method
+function sendaudio($chat, $audio, $caption, $reply = '') {
+    return bot('sendPhoto', [
+        'chat_id' => $chat,
+        'audio' => $audio,
+		'caption' => $caption,
+		'reply_to_message_id' => $reply,
+		'parse_mode' => 'markdown'
+    ]);
+}
+// sendSticker method
+function sendsticker($chat, $sticker, $reply = '') {
+    return bot('sendSticker', [
+        'chat_id' => $chat,
+		'sticker' => $sticker,
+		'reply_to_message_id' => $reply,
+    ]);
+}
+// sendVideo method
+function sendvideo($chat, $video, $caption, $reply = '') {
+    return bot('sendVideo', [
+        'chat_id' => $chat,
+        'video' => $video,
+		'caption' => $caption,
+		'reply_to_message_id' => $reply,
+		'parse_mode' => 'markdown'
+    ]);
+}
 
 
 ?>
